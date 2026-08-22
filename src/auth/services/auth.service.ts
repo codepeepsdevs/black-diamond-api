ac0d851c9a38fa30aacfeb9a03f1a47845c28128
Emmanuel <139286913+imDmanuel@users.noreply.github.com>
2026-08-22 09:15:15 +0100

    chore: replace bcrypt with bcryptjs

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -7,7 +7,7 @@ import {
   NotFoundException,
   UnauthorizedException,
 } from '@nestjs/common';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import { JwtService, TokenExpiredError } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 import { PrismaService } from 'src/prisma/prisma.service';
